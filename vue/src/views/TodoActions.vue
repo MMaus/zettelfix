513476df947f565087411a32d3f56b3e6b01472e
Moritz Maus <h.moritz.maus@gmail.com>
2021-12-30 22:01:07 +0100

    Minor: name change

--- vue/src/views/TodoActions.vue
+++ vue/src/views/TodoActions.vue
@@ -2,11 +2,15 @@
   <w-flex wrap>
     <div class="spacer"></div>
     <sync-button module="todo"></sync-button>
-    <w-button shadow @click="showIt = true">
+    <w-button shadow @click="showDialog = true">
       <w-icon lg>mdi mdi-newspaper-plus</w-icon></w-button
     >
   </w-flex>
-  <add-todo-dialog v-model:dialogOpen="showIt" @close="createItem">
+  <add-todo-dialog
+    v-model:dialogOpen="showDialog"
+    transition="slide-fade-down"
+    @close="createItem"
+  >
   </add-todo-dialog>
 </template>
 
@@ -18,11 +22,11 @@ import SyncButton from "@/components/common/SyncButton.vue";
 
 const store = useStore();
 
-const showIt = ref(false);
+const showDialog = ref(false);
 function createItem(label: string) {
   if (label) {
     store.dispatch("todo/createTodoItem", label);
   }
-  showIt.value = false;
+  showDialog.value = false;
 }
 </script>
