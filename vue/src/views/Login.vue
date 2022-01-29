a0f502ad29749ea241966de8fdaaed107f43509c
Moritz Maus <h.moritz.maus@gmail.com>
2022-01-29 20:10:48 +0100

    Start refactoring Login/Account page

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
+    <w-card
+      title="Acccount settings"
+      title-class="indigo-light4--bg"
+      class="mx2 text-left"
+    >
+      <h4 class="mb2">Account (Email)</h4>
+      {{ accountName }}
+      <w-divider class="my2" />
+      <h4 class="mb2">User details</h4>
+      <w-input label="Name"></w-input>
+      <w-input label="Phone"></w-input>
+      <template #actions>
+        <div class="spacer"></div>
+        <w-button @click="showChangePasswordDialog = true"
+          >Change Password</w-button
+        >
+        <w-button>Save</w-button>
+      </template>
+    </w-card>
+    <change-password-dialog
+      v-model:showDialog="showChangePasswordDialog"
+    ></change-password-dialog>
+  </div>
+  <!-- <div>
     <div class="jumbotron">
       <h1>Login</h1>
       <div v-if="loggedIn">
@@ -41,75 +65,14 @@
       <h4 class="text-white">Success!</h4>
       {{ successMsg }}
     </div>
-  </div>
+  </div> -->
 </template>
 
-<script lang="ts">
-import { ref, onMounted } from "vue";
-
-export default {
-  setup() {
-    const email = ref("");
-    const password = ref("");
-    const showError = ref(false);
-    const errMsg = ref("");
-    const showSuccess = ref(false);
-    const successMsg = ref("");
-    const userVerified = ref(false);
-    const loggedIn = ref(false);
-    const currentUser = ref("");
-
-    const displaySuccess = function () {
-      showError.value = false;
-      showSuccess.value = true;
-      successMsg.value = "Ois guad!";
-    };
-
-    const displayError = function (error: any): void {
-      console.log("received error: " + error);
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      showError.value = true;
-      showSuccess.value = false;
-      errMsg.value = errorMessage;
-      console.log("error displayed: " + error);
-    };
-
-    const login = function () {
-      console.log("login clicked");
-    };
-
-    const signUp = function () {
-      console.log("signing up ... currently rewritten");
-    };
-
-    const sendVerification = function () {
-      console.log("signing up ... currently rewritten");
-    };
-
-    const logout = function () {
-      console.log("logout clicked");
-    };
-
-    return {
-      email,
-      password,
-      showError,
-      errMsg,
-      showSuccess,
-      successMsg,
-      userVerified,
-      loggedIn,
-      currentUser,
-      signUp,
-      login,
-      logout,
-      sendVerification,
-      displayError,
-      displaySuccess,
-    };
-  },
-
-  components: {},
-};
+<script setup lang="ts">
+import { ref } from "vue";
+import ChangePasswordDialog from "@/components/user/ChangePasswordDialog.vue";
+import { useStore } from "vuex";
+const showChangePasswordDialog = ref(false);
+const store = useStore();
+const accountName = store.getters["app/user"] || "Not logged in";
 </script>
