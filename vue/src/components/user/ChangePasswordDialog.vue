3f0cbe945c754da5bc7dacd7f872c773d2f8e3ca
Moritz Maus <h.moritz.maus@gmail.com>
2022-01-30 13:31:40 +0100

    Add ChangePasswordDialog component (missed)

---
+++ vue/src/components/user/ChangePasswordDialog.vue
@@ -0,0 +1,26 @@
+<template>
+  <w-dialog v-model="dialogOpen"> Change Password here! </w-dialog>
+</template>
+<script setup lang="ts">
+import { ref, defineProps, defineEmits, watch } from "vue";
+const dialogOpen = ref(false);
+const props = defineProps<{
+  showDialog: boolean;
+}>();
+const emit = defineEmits<{
+  (e: "update:showDialog", value: boolean): void;
+}>();
+
+watch(
+  () => props.showDialog,
+  (val) => {
+    dialogOpen.value = val;
+  }
+);
+watch(
+  () => dialogOpen.value,
+  (val) => {
+    emit("update:showDialog", val);
+  }
+);
+</script>
