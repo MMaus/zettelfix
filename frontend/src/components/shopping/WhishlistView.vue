de1e8e25d3f73dcfc14c66b98d4530f4613bfd0e
Moritz Maus <moritz.maus@hm10.net>
2023-01-18 07:34:31 +0100

    feat(#36): add draft of 'add item to whishlist' UI

--- frontend/src/components/shopping/WhishlistView.vue
+++ frontend/src/components/shopping/WhishlistView.vue
@@ -1,5 +1,61 @@
 <template>
   <w-card shadow title="Whishlist" title-class="amber-light5--bg">
+    <template #title>
+      <w-flex wrap>
+        <div class="bordered mr5">
+          <span class="title2 ma2">Whishlist</span>
+        </div>
+        <div class="bordered">
+          <w-input label="add item"></w-input>
+        </div>
+        <div class="bordered">
+          <w-input label="search Item" @focus="showDrawer = true"> </w-input>
+          <div
+            v-show="showDrawer"
+            class="bordered testfloat"
+            style="height: 40vh; width: 60vw; z-index: 200"
+          >
+            <w-drawer
+              absolute
+              top
+              no-overlay
+              v-model="showDrawer"
+              height="40vh"
+              @click="showDrawer = false"
+            >
+              <w-button
+                @click="showDrawer = false"
+                icon="wi-cross"
+                sm
+                outline
+                round
+                absolute
+              >
+              </w-button>
+              <w-flex><div class="spacer"></div></w-flex>
+              <w-list
+                class="full-width"
+                :items="testItems"
+                v-model="selectedItem"
+              >
+                <template #item="{ item }">
+                  <w-flex>
+                    <div class="bordered">
+                      <div class="flex" @click.stop="">
+                        <w-button>-</w-button>
+                        <w-input class="narrow" v-model="item.qty"></w-input>
+                        <w-button>+</w-button>
+                      </div>
+                    </div>
+                    <div class="ml3">{{ item.label }}</div>
+                  </w-flex>
+                </template>
+              </w-list>
+            </w-drawer>
+          </div>
+        </div>
+      </w-flex>
+    </template>
     <w-input
       v-model="searchText"
       class="mb2"
@@ -27,10 +83,14 @@
   </w-card>
 </template>
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, ref, type Ref } from "vue";
 import { type UUID, useShoppingStore } from "./shoppingStore";
 
+const showList = ref(false);
+
+const selectedItem = ref(null) as Ref<any>;
 const searchText = ref("");
+const showDrawer = ref(false);
 
 const selectedRows = ref([]);
 
@@ -46,6 +106,12 @@ const whishlistHeaders = [
 ];
 
 const store = useShoppingStore();
+
+const testItems = ref([
+  { label: "foo", qty: "0" },
+  { label: "bar", qty: "3" },
+  { label: "baz", qty: 4 },
+]);
 
 // store.addItemToWhishlist(store.items[2].id);
 
@@ -72,3 +138,20 @@ const whishlistItemData = computed(() => {
   });
 });
 </script>
+<style scoped>
+.testfloat {
+  position: absolute;
+}
+
+.full-width {
+  width: 100%;
+}
+
+.narrow {
+  width: 32px;
+}
+
+.flex {
+  display: flex;
+}
+</style>
