a80bac7a4a92dba96229a40335881a22fe9bad55
Moritz Maus <moritz.maus@hm10.net>
2023-01-25 07:25:12 +0100

    feat(#62): select shop for shopping

--- frontend/src/components/shopping/ShoppingNowView.vue
+++ frontend/src/components/shopping/ShoppingNowView.vue
@@ -1,36 +1,48 @@
 <template>
-  <w-card shadow title="Shopping now" title-class="amber-light5--bg">
-    <div class="growcontainer">
-      <div class="largeflex">
-        <div class="scroll">
-          <div class="demo sticky-top">Add shop</div>
-          <ul>
-            <li>foo</li>
-            <li v-for="dat in data" :key="dat" class="my3">{{ dat }}</li>
-            <li>bar</li>
-          </ul>
-          bar
-        </div>
-      </div>
-    </div>
+  <w-card shadow title="Shopping" title-class="amber-light5--bg">
+    <w-select
+      :items="shops"
+      round
+      shadow
+      :inner-icon-left="!!selectedShop ? 'mdi mdi-close-circle' : undefined"
+      @click:inner-icon-left="selectedShop = ''"
+      label="Select shop"
+      class="selectbox"
+      v-model="selectedShop"
+    >
+    </w-select>
+    {{ selectedShop }}
+    {{ shelves }}
   </w-card>
 </template>
 <script setup lang="ts">
-const data = [
-  "foo",
-  "bar",
-  "baz",
-  "dfs",
-  "fofo",
-  "dada",
-  "bla",
-  "blubb",
-  "lorem",
-  "dolor",
-  "ipsum",
-];
+import { computed, ref } from "vue";
+import { useShoppingStore } from "./shoppingStore";
+
+const store = useShoppingStore();
+
+const shelves = computed(() => {
+  if (!selectedShop) {
+    return [];
+  }
+  return store.getShelves(selectedShop.value);
+});
+
+const selectedShop = ref("");
+const shops = computed(() =>
+  store.shops.map((sh) => {
+    return {
+      label: sh.name,
+      value: sh.id,
+    };
+  })
+);
 </script>
 <style scoped>
+.selectbox {
+  max-width: 600px;
+  width: fit-content;
+}
 .demo {
   background-color: khaki;
 }
