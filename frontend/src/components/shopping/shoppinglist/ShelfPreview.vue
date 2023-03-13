76ca9a96fcee2a2e4750c977f3fbb1e0da19a9f4
Moritz Maus <moritz.maus@hm10.net>
2023-03-13 07:34:54 +0100

    feat: proper shopping shelf preview

--- frontend/src/components/shopping/shoppinglist/ShelfPreview.vue
+++ frontend/src/components/shopping/shoppinglist/ShelfPreview.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="px2">
-    <w-card shadow :title="props.shelfpreview.name" class="my2">
-      <div v-for="item in shelfpreview.items">
-        {{ item }}
-      </div>
+    <w-card
+      shadow
+      :title="props.shelfpreview.name"
+      class="my2 light-green-light6--bg"
+    >
+      <w-table :headers="tableHeaders" :items="itemData"> </w-table>
     </w-card>
   </div>
 </template>
 <script setup lang="ts">
+import { computed } from "vue";
 import { ShoppingShelfPreview } from "../shoppingStore";
 
 const props = defineProps<{ shelfpreview: ShoppingShelfPreview }>();
+
+const tableHeaders = [
+  { label: "Qty", key: "amount", width: "72px" },
+  { label: "Item", key: "name" },
+];
+const itemData = computed(() =>
+  props.shelfpreview.items.map((pv) => {
+    return {
+      id: pv.item.id,
+      amount: pv.amount,
+      name: pv.item.name,
+    };
+  })
+);
 </script>
